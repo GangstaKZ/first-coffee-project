fc4a97fba525857e5acaa050851321dd252087ed
GangstaKZ <57038119+GangstaKZ@users.noreply.github.com>
2025-09-13 14:15:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById('burger');
+const navList = document.querySelector('.nav-list');
+
+burger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+});
